8b45a120bf82b4e5a53e8f16b782d8dd2702ff51
Gil Forcada <gforcada@gnome.org>
2015-08-16 16:53:52 +0200

    No need for regular files, make them on the fly

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from flake8_plone_api import PloneAPIChecker
+from tempfile import mkdtemp
 
+import os
 import unittest
 
 
 class TestFlake8PloneAPI(unittest.TestCase):
 
+    def _given_a_file_in_test_dir(self, contents):
+        test_dir = os.path.realpath(mkdtemp())
+        file_path = os.path.join(test_dir, 'test.py')
+        with open(file_path, 'w') as a_file:
+            a_file.write(contents)
+
+        return file_path
+
     def test_get_replacement(self):
-        checker = PloneAPIChecker(None, 'test_file.py')
+        file_path = self._given_a_file_in_test_dir(
+            'from somewhere import getToolByName'
+        )
+        checker = PloneAPIChecker(None, file_path)
         ret = list(checker.run())
         self.assertEqual(len(ret), 1)
         self.assertEqual(ret[0][0], 1)
@@ -15,7 +28,10 @@ class TestFlake8PloneAPI(unittest.TestCase):
         self.assertTrue(ret[0][2].startswith('P001 found '))
 
     def test_no_replacement_for_xxx(self):
-        checker = PloneAPIChecker(None, 'test_file_xxx.py')
+        file_path = self._given_a_file_in_test_dir(
+            'from somewhere import XXX'
+        )
+        checker = PloneAPIChecker(None, file_path)
         ret = list(checker.run())
         self.assertEqual(ret, [])
 
--- test_file_xxx.py
+++
@@ -1,1 +0,0 @@
-from somewhere import XXX
